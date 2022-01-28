36d127259994cae2e4a7b2c34b105027deaedc1a
JuliaM88 <julia.moskalenko@outlook.com>
2022-01-28 20:42:29 +0100

    fix taskOne

--- TaskOne/PhoneNumberReader.java
+++ TaskOne/PhoneNumberReader.java
@@ -3,6 +3,8 @@ package TaskOne;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PhoneNumberReader {
     private FileInputStream fis;
@@ -39,15 +41,29 @@ public class PhoneNumberReader {
         return bufferArray;
     }
 
-    public boolean isPhoneNumberOk(String data) {
-        String dataWithoutSpaces = data.replace(" ", "");
-        return dataWithoutSpaces.length() > 10;
+//    public boolean isPhoneNumberOk(String data) {
+//        String dataWithoutSpaces = data.replace(" ", "");
+//        return dataWithoutSpaces.length() > 10;
+//    }
+
+    public boolean isPhoneNumberValid(String data) {
+        //987-123-4567
+        Pattern patternOne = Pattern.compile("\\d{3}-\\d{3}-\\d{4}");
+
+        //(123) 456-7890
+        Pattern patternTwo = Pattern.compile("\\(\\d{3}\\)\\s\\d{3}-\\d{4}");
+
+        Matcher matcherOne = patternOne.matcher(data);
+        Matcher matcherTwo = patternTwo.matcher(data);
+
+        return matcherOne.matches() || matcherTwo.matches();
     }
+
 
     public void printAllPhoneNumber() {
         List<String> listOfPhoneNumbers = readAllLines();
         for (int i = 0; i < listOfPhoneNumbers.size(); i++) {
-            if (isPhoneNumberOk(readAllLines().get(i))) {
+            if (isPhoneNumberValid(readAllLines().get(i))) {
                 System.out.println(readAllLines().get(i));
             }
         }
